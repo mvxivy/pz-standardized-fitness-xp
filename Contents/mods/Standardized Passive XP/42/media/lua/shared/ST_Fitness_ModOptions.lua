7237afc09e82292db54edc50156aea2fdc34ec01
Maksim Ivanov <vonavimax@gmail.com>
2025-01-25 21:41:25 +0700

    feat: mod options

--- Contents/mods/Standardized Passive XP/42/media/lua/shared/ST_Fitness_ModOptions.lua
+++ Contents/mods/Standardized Passive XP/42/media/lua/shared/ST_Fitness_ModOptions.lua
@@ -1,71 +1,61 @@
---Default options.
-local SETTINGS = { 
-	options = {
-		dropdown1 = 1,
-		dropdown2 = 1,
-		dropdown3 = 1,
-		dropdown4 = 1,
-		dropdown5 = 1,
-	},
-	names= {
-		dropdown1 = "Fitness XP Multiplier",
-		dropdown2 = "Fitness XP Chance (One in X chance (1/x))",
-		dropdown3 = "Strength XP Multiplier",
-		dropdown4 = "Strength XP Chance (One in X chance (1/x))",
-		dropdown5 = "Trees require weapons to give strength XP",
-	},
-	mod_id = "STPASSIVE",
-	mod_shortname = "Standardized Passive XP"
-}
+local config = require("ST_Fitness_Config")
+local MVXIVY_Utils = require("MVXIVY_Utils")
+local modOptions = {}
 
--- Connecting the options to the menu, so user can change them.
-if ModOptions and ModOptions.getInstance then
-	local settings = ModOptions:getInstance(SETTINGS)
-	
-	local drop1 = settings:getData("dropdown1")
-	drop1[1] = "1xp - Vanilla"
-	drop1[2] = "2xp"
-	drop1[3] = "3xp"
-	drop1[4] = "4xp"
-	drop1[5] = "5xp"
-	drop1[6] = "10xp"
-	drop1[7] = "15xp"
-	drop1[8] = "25xp"
-	drop1[9] = "50xp"
-	drop1[10] = "100xp"
-	drop1.tooltip = "Xp Multiplier for passive Fitness XP.(that means NOT training XP)"
-	local drop2 = settings:getData("dropdown2")
-	drop2[1] = "200"
-	drop2[2] = "300"
-	drop2[3] = "500"
-	drop2[4] = "700 - Vanilla"
-	drop2[5] = "1000"
-	drop2.tooltip = "Chance to obtain Fitness XP, as a one in x chance (1/x). Setting to 200 means 1/200 chance. Vanilla value is 700."
-	local drop3 = settings:getData("dropdown3")
-	drop3[1] = "1xp"
-	drop3[2] = "2xp - Vanilla"
-	drop3[3] = "3xp"
-	drop3[4] = "4xp"
-	drop3[5] = "5xp"
-	drop3[6] = "10xp"
-	drop3[7] = "15xp"
-	drop3[8] = "25xp"
-	drop3[9] = "50xp"
-	drop3[10] = "100xp"
-	drop3.tooltip = "Xp Multiplier for passive Strength XP.(that means NOT training XP)"
-	local drop4 = settings:getData("dropdown4")
-	drop4[1] = "200"
-	drop4[2] = "300"
-	drop4[3] = "500"
-	drop4[4] = "700 - Vanilla"
-	drop4[5] = "1000"
-	drop4.tooltip = "Chance to obtain Strength XP, as a one in x chance (1/x). Setting to 200 means 1/200 chance. Vanilla value is 700."
-	local drop5 = settings:getData("dropdown5")
-	drop5[1] = "Yes. I must have a weapon."
-	drop5[2] = "No. I want to become Steve from minecraft."
-	drop5.tooltip = "When a player hits a tree, they are given strength experience as long as they are not using their bare hands.  This allows you to enable using your bare hands in addition to weapons."
+function modOptions.init()
+  local UI = PZAPI.ModOptions:create(config.modId, config.modName)
 
+  local ComboBoxFactory = MVXIVY_Utils.useComboBoxFactory(
+    "ST_Fitness_",
+    "UI_options_" .. config.modId .. "_",
+    UI
+  )
+
+  local ComboBoxFitnessXP = ComboBoxFactory {
+    name = "FitnessXP",
+    label = "fitness_xp_label",
+    items = {"1xp", "2xp", "3xp", "4xp", "5xp", "10xp", "15xp", "25xp", "50xp", "100xp"},
+    defaultItem = 1,
+    description = "fitness_xp_description"
+  }
+  modOptions.ComboBoxFitnessXP = ComboBoxFitnessXP
+
+  local ComboBoxFitnessChance = ComboBoxFactory {
+    name = "FitnessChance",
+    label = "fitness_chance_label",
+    items = {"1/200", "1/300", "1/500", "1/700", "1/1000"},
+    defaultItem = 4,
+    description = "fitness_chance_description"
+  }
+  modOptions.ComboBoxFitnessChance = ComboBoxFitnessChance
+
+  local ComboBoxStrengthXP = ComboBoxFactory {
+    name = "StrengthXP",
+    label = "strength_xp_label",
+    items = {"1xp", "2xp", "3xp", "4xp", "5xp", "10xp", "15xp", "25xp", "50xp", "100xp"},
+    defaultItem = 1,
+    description = "strength_xp_description"
+  }
+  modOptions.ComboBoxStrengthXP = ComboBoxStrengthXP
+
+  local ComboBoxStrengthChance = ComboBoxFactory {
+    name = "StrengthChance",
+    label = "strength_chance_label",
+    items = {"1/200", "1/300", "1/500", "1/700", "1/1000"},
+    defaultItem = 4,
+    description = "strength_chance_description"
+  }
+  modOptions.ComboBoxStrengthChance = ComboBoxStrengthChance
+
+  local ComboBoxGachi = ComboBoxFactory {
+    name = "Gachi",
+    label = "gachi_label",
+    items = {getText("UI_options_STPASSIVE_B42_gachi_option_yes"),
+             getText("UI_options_STPASSIVE_B42_gachi_option_no")},
+    defaultItem = 1,
+    description = "gachi_description"
+  }
+  modOptions.ComboBoxGachi = ComboBoxGachi
 end
 
-StandardizedPassiveXP_global = {}
-StandardizedPassiveXP_global.SETTINGS = SETTINGS
+return modOptions
